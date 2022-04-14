b43c720c10ec676c73bdb8a70d04a2d7a9075e41
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-04-14 22:53:38 +0600

    login-fixed

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () => {
         navigate('/login')
     }
 
+    if(user){
+        navigate('/home');
+    }
     const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
 
-        
+        createUserWithEmailAndPassword(email, password);
+
     }
 
     return (
         <div className='register-form'>
             <h2 style={{textAlign: "center"}}>Please Register</h2>
-            <form>
+            <form onSubmit={handleRegister}>
             <input type="text" name="name" id="" placeholder='Your Name'/>
             <input type="email" name="email" id="" placeholder='Email Address' required/>
             <input type="password" name="password" id="" placeholder='Password' required/>
